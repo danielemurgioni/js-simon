522163f33c9b0ee8d28b37734b71236536137956
danielemurgioni <dashomew@gmail.com>
2025-03-27 17:02:40 +0100

    [UPD] select required DOM elements and apply the program to it

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-// creo un ciclo for per creare 5 numeri casuali che inserisco dentro un'array
+// elementi DOM e variabili
 
 let randomNumbers = []
+let userNumbers = []
+let result = []
+let numberList = document.getElementById("numbers-list");
+let visibleForm = document.getElementById("answers-form");
+let inputGroup = document.querySelectorAll("div#input-group > input");
+let button = document.querySelector("button");
+
+console.log(numberList, inputGroup, visibleForm, button);
+
+// creo un ciclo for per creare 5 numeri casuali che inserisco dentro un'array
 
 for(let i=0; i<5; i++){
     randomNumbers.push(rN(1, 50));
@@ -8,29 +18,34 @@ for(let i=0; i<5; i++){
 
 console.log(randomNumbers);
 
-// creo un ciclo for per chiedere all'utente 5 numeri che inserisco dentro un'array
+//prendo i risultati del form quando premo il bottone
 
-let userNumbers = []
+button.addEventListener("click", (event) => {
+    event.preventDefault();
 
-for(let i=0; i<5; i++){
-    userNumbers.push(parseInt(prompt("Inserisci i numeri")));
-}
+    //ottengo i valori degli input e li pusho dentro l'array userNumbers
 
-console.log(userNumbers);
+    for(let i=0; i<inputGroup.length; i++){
+        userNumbers.push(parseInt(inputGroup[i].value));
+    }
+    console.log(userNumbers);
 
-// con due cicli for controllo gli elementi di due array, il risultato deve contenere solo i numeri dell'utente che sono uguali a quelli generati
+    // con due cicli for controllo gli elementi di due array, il risultato deve contenere solo i numeri dell'utente che sono uguali a quelli generati
 
-let result = []
-
-for(let i=0; i<randomNumbers.length; i++){
-    for(let y=0; y<userNumbers.length; y++){
-        if (randomNumbers[i] === userNumbers[y]){
-            result.push(userNumbers[y]);
+    for(let i=0; i<randomNumbers.length; i++){
+        for(let y=0; y<userNumbers.length; y++){
+            if (randomNumbers[i] === userNumbers[y]){
+                result.push(userNumbers[y]);
+            }
         }
     }
-}
+    console.log(result);
+})
 
-console.log(result);
+//cambiamenti nel DOM
+
+numberList.innerHTML = `<li>${randomNumbers}</li>`;
+visibleForm.classList.remove("d-none");
 
 //Functions
 
